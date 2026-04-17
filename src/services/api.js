a59c99e1c1c373da6daac6f57f9c561945694cd3
diego castellanos <diego.castellanos@scitisgroup.com>
2026-04-17 16:33:45 -0500

    cambios en response api

--- src/services/api.js
+++ src/services/api.js
@@ -3,16 +3,24 @@ const API_URL = process.env.REACT_APP_API_URL;
 console.log("URL final:", process.env.REACT_APP_API_URL);
 
 export const sendQueryToLambda = async (text) => {
-  const response = await fetch(process.env.REACT_APP_API_URL, {
+  const response = await fetch(API_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      query: text,
-    }),
+    body: JSON.stringify({ query: text }),
   });
 
-  const data = await response.json();
-  return data.results;
+  const lambdaResponse = await response.json();
+  const data = JSON.parse(lambdaResponse.body);
+
+  if (data.type === "prediction") {
+    return data.data;
+  }
+
+  if (data.type === "rag") {
+    return data.data;
+  }
+
+  return "No response";
 };
