a0ce3699f55e5e23a46f123bb0cefa103e0624ea
diego castellanos <diego.castellanos@scitisgroup.com>
2026-04-17 14:37:11 -0500

    adicion a parametro URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
-console.log("URL final:", ENV.REACT_APP_API_URL);
+console.log("URL final:", process.env.REACT_APP_API_URL);
 
 export const sendQueryToLambda = async (text) => {
   const response = await fetch(process.env.REACT_APP_API_URL, {
