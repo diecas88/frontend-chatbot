a044ae6fa7be26100801903c76fae48de1afb40a
diego castellanos <diego.castellanos@scitisgroup.com>
2026-04-20 10:44:09 -0500

    cambios en api.js para obtener respuesta de lambda

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,11 @@
-const API_URL = process.env.REACT_APP_API_URL;
+//import ENV from "../config/env";
+//console.log("URL final:", ENV.API_URL);
 
-console.log("URL final:", process.env.REACT_APP_API_URL);
+const API_URL = process.env.REACT_APP_API_URL;
+//console.log("URL final:", process.env.REACT_APP_API_URL);
 
 export const sendQueryToLambda = async (text) => {
+  //const response = await fetch(ENV.API_URL, {
   const response = await fetch(API_URL, {
     method: "POST",
     headers: {
@@ -12,15 +15,22 @@ export const sendQueryToLambda = async (text) => {
   });
 
   const lambdaResponse = await response.json();
-  const data = JSON.parse(lambdaResponse.body);
+  console.log("respuesta===>", lambdaResponse);
 
-  if (data.type === "prediction") {
-    return data.data;
+  try {
+    if (lambdaResponse.type == "prediction") {
+      return "Esto es una predicción, " + lambdaResponse.data;
+    }
+
+    if (lambdaResponse.type == "rag") {
+      return "Esto es dato histórico, " + lambdaResponse.data;
+    }
+
+    return "No response";
+  }
+  catch(e) {
+
+    return e;
   }
 
-  if (data.type === "rag") {
-    return data.data;
-  }
-
-  return "No response";
 };
