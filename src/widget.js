eaa760f6519070346647984c4976a122939302c3
diego castellanos <diego.castellanos@scitisgroup.com>
2026-04-24 14:09:49 -0500

    fix: agregar css tailwind

--- src/widget.js
+++ src/widget.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ChatWidget from "./components/ChatWidget";
+import "./index.css";
 
 (function () {
   // Evita duplicarlo
